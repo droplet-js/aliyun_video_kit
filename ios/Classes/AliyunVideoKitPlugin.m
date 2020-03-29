bfa6784e817e9f752f0b2e3a5e419c2c644fc087
v7lin <v7lin@qq.com>
2020-03-29 19:36:20 +0800

    初始化

---
+++ ios/Classes/AliyunVideoKitPlugin.m
@@ -0,0 +1,20 @@
+#import "AliyunVideoKitPlugin.h"
+
+@implementation AliyunVideoKitPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"aliyun_video_kit"
+            binaryMessenger:[registrar messenger]];
+  AliyunVideoKitPlugin* instance = [[AliyunVideoKitPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
