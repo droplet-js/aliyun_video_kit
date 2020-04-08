b1765ebea9da3ef826507cf4df8b08df0ebc4d11
v7lin <v7lin@qq.com>
2020-04-08 23:45:42 +0800

    优化

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:aliyun_video_kit/aliyun_video_kit.dart';
 
